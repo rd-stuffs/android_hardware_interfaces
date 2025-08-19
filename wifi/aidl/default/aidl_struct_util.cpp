e5757c92593ff065b929a105d251b6b4e40d33bf
Nate Jiang <qiangjiang@google.com>
2025-08-19 10:56:51 +0000

    Pass the info to vendor to enable frame protection
    Bug: 440147300
    Test: build & multi device test
    Flag: BUGFIX
    CRs-Fixed: 4362717
    Change-Id: I1805d3f5d624715f20218524cf86ebfd38a64c45
    (cherry picked from commit 5c928d859ced0ddf3196504ef9066bace5e3185e)

--- wifi/aidl/default/aidl_struct_util.cpp
+++ wifi/aidl/default/aidl_struct_util.cpp
@@ -1853,6 +1853,8 @@ bool convertAidlNanPublishRequestToLegacy(const NanPublishRequest& aidl_request,
             (aidl_request.baseConfigs.securityConfig.securityType != NanDataPathSecurityType::OPEN)
                     ? legacy_hal::NAN_DP_CONFIG_SECURITY
                     : legacy_hal::NAN_DP_CONFIG_NO_SECURITY;
+    legacy_request->sdea_params.gtk_protection =
+            aidl_request.baseConfigs.securityConfig.requiresEnhancedFrameProtection ? 1 : 0;
 
     legacy_request->sdea_params.ranging_state = aidl_request.baseConfigs.rangingRequired
                                                         ? legacy_hal::NAN_RANGING_ENABLE
@@ -1988,6 +1990,8 @@ bool convertAidlNanSubscribeRequestToLegacy(const NanSubscribeRequest& aidl_requ
             (aidl_request.baseConfigs.securityConfig.securityType != NanDataPathSecurityType::OPEN)
                     ? legacy_hal::NAN_DP_CONFIG_SECURITY
                     : legacy_hal::NAN_DP_CONFIG_NO_SECURITY;
+    legacy_request->sdea_params.gtk_protection =
+            aidl_request.baseConfigs.securityConfig.requiresEnhancedFrameProtection ? 1 : 0;
     legacy_request->sdea_params.ranging_state = aidl_request.baseConfigs.rangingRequired
                                                         ? legacy_hal::NAN_RANGING_ENABLE
                                                         : legacy_hal::NAN_RANGING_DISABLE;
